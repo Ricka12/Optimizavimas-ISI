32755d434cf835caf8221bf95431ba3e761f9270
Ricka12 <ricgembickis@gmail.com>
2024-04-10 02:08:24 +0300

    Padaryti pakeitimai

--- Optimizavimas2.cpp
+++ Optimizavimas2.cpp
@@ -1,24 +1,24 @@
-#include <iostream>
+﻿#include <iostream>
 #include <fstream>
 using namespace std;
 
 ofstream R("Octave.txt");
 /////////////////////////////////////////////////////////////////////////////////////////////////
-struct duKint {
+struct duKint { //Vartotojo apibreztas kintamasis kad galima butu perduoti 2 kintamuosius
 	double xNaujas1, xNaujas2;
 };
 /////////////////////////////////////////////////////////////////////////////////////////////////
-bool hessePatikra(double x2, double x3)
+bool hessePatikra(double x2, double x3) //gal implementuosiu dar nezinau ar reikia !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 {
 	return 2 * x3 >= 0 && 2 * x2 + 2 * x3 - 1 >= 0 && 2 * x2 >= 0;//grazina 1 jei hesse matrica teigiamai apibrezta
 }
 
-double tiksloFunkcija(double x2, double x3)
+double tiksloFunkcija(double x2, double x3) //tikslo funkcija su kuria yra dirbama
 {
 	return (x2 * x2 * x3 + x2 * x3 * x3 - x2 * x3) / 8;
 }
 /////////////////////////////////////////////////////////////////////////////////////////////////
-duKint gradientas(duKint x)
+duKint gradientas(duKint x) // skaiciuojamas gradientas nuo tasku
 {
 	duKint a;
 	a.xNaujas1 = (2 * x.xNaujas1 * x.xNaujas2 + x.xNaujas2 * x.xNaujas2 - x.xNaujas2) / 8;
@@ -26,14 +26,83 @@ duKint gradientas(duKint x)
 
 	return a;
 }
+/////////////////////////////////////////////////////////////////////////////////////////////////Intervalo dalijimo pusiau metodas
+double skaiciuojamFunkcija(double x)
+{
+	return ((x * x - 2) * (x * x - 2)) / 4 - 1;
+}
+////////////////////// Kaip pasiskaiciuot gamma?? kas yra gama siuo atveju?? !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+void intervaloPusiau()
+{
+	int pabaiga = 0, skaiciavimuKiekisDalijimoPusiau = 0, iteracijuSkDalPus = 0;
+	double l = 0, r = 10;
+	double xm = (l + r) / 2;
+	double L = r - l;
+	double x1, x2;
+	double fxm, fx1, fx2;
+	fxm = skaiciuojamFunkcija(xm);
+	skaiciavimuKiekisDalijimoPusiau++;
+	while (pabaiga != 1)
+	{
+		iteracijuSkDalPus++;
+		x1 = l + L / 4;
+		x2 = r - L / 4;
+		fx1 = skaiciuojamFunkcija(x1);
+		fx2 = skaiciuojamFunkcija(x2);
+		skaiciavimuKiekisDalijimoPusiau += 2;
+		if (fx1 < fxm)
+		{
+			r = xm;
+			xm = x1;
+			fxm = fx1;
+		}
+		else if (fx2 < fxm)
+		{
+			l = xm;
+			xm = x2;
+			fxm = fx2;
+
+		}
+		else {
+			l = x1;
+			r = x2;
+		}
+		L = r - l;
+		if (L < 0.0001)
+		{
+			pabaiga = 1;
+		}
+	}
+}
 /////////////////////////////////////////////////////////////////////////////////////////////////
 void gradientinisNusileidimas(duKint a)
 {
 	duKint zingsnis = a;
-	double gamma = 0.1;
+	double gamma = 0.75;
 	int pabaiga = 0;
 	while (pabaiga != 1)
 	{
+		zingsnis.xNaujas1 = a.xNaujas1;
+		zingsnis.xNaujas2 = a.xNaujas2;
+		R << "plot3(" << a.xNaujas1 << ", " << a.xNaujas2 << ", " << tiksloFunkcija(a.xNaujas1, a.xNaujas2) << ",'ro', 'MarkerSize', 5)" << endl;// Vizualizcijai su octave
+		a.xNaujas1 = a.xNaujas1 - gamma * gradientas(a).xNaujas1;
+		a.xNaujas2 = a.xNaujas2 - gamma * gradientas(a).xNaujas2;
+		if (zingsnis.xNaujas1 - a.xNaujas1 <= 0.0000000001 && zingsnis.xNaujas2 - a.xNaujas2 <= 0.0000000001)//Iki kokio min zingsnio turi eiti?? !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+		{
+			pabaiga = 1;
+		}
+	}
+}
+/////////////////////////////////////////////////////////////////////////////////////////////////
+void greiciausiasNusileidimas(duKint a)
+{
+	duKint zingsnis = a;
+	double gamma;
+	int pabaiga = 0;
+	srand(time(NULL));
+	while (pabaiga != 1)
+	{
+		gamma = rand()%1+1; //!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 		zingsnis.xNaujas1 = a.xNaujas1;
 		zingsnis.xNaujas2 = a.xNaujas2;
 		R << "plot3(" << a.xNaujas1 << ", " << a.xNaujas2 << ", " << tiksloFunkcija(a.xNaujas1, a.xNaujas2) << ",'ro', 'MarkerSize', 5)" << endl;// Vizualizcijai su octave
@@ -46,9 +115,6 @@ void gradientinisNusileidimas(duKint a)
 	}
 }
 /////////////////////////////////////////////////////////////////////////////////////////////////
-// 
-/////////////////////////////////////////////////////////////////////////////////////////////////
-
 int main()
 {
 	//////////////////////////////////////////////////////////////////////////////////// Funkcijos vizualizacija
@@ -69,14 +135,23 @@ int main()
 	duKint a{0,0};
 	duKint b{ 1,1 };
 	duKint c{0.2,0.4};
-	R << "% Taskas(0, 0)" << endl;// 
+	R << "% Taskas(0, 0)" << endl;
 	gradientinisNusileidimas(a);
-	R << "% Taskas(1, 1) " << endl;// 
+	R << "% Taskas(1, 1) " << endl;
 	gradientinisNusileidimas(b);
-	R << "% Taskas(0.2, 0.4) " << endl;// 
+	R << "% Taskas(0.2, 0.4) " << endl;
 	gradientinisNusileidimas(c);
 	////////////////////////////////////////////////////////////////////////////////////greiciausiasNusileidimas
-
+	R << "% Taskas(0, 0)" << endl;
+	greiciausiasNusileidimas(a);
+	R << "% Taskas(1, 1) " << endl;
+	greiciausiasNusileidimas(b);
+	R << "% Taskas(0.2, 0.4) " << endl;
+	greiciausiasNusileidimas(c);
 	////////////////////////////////////////////////////////////////////////////////////deformuotasSimplekso
-
+	/*
+	Sudaromas pradinis simpleksas ir visose jo viršun¯ ese ˙
+apskaiciuojamos tikslo funkcijos reikšm ˇ es. Simplekso dydi˛ ˙
+reguliuoja parametras α. a parenkamas pacio zmogaus??
+*/
 }
