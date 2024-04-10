f23a6ea12a814f156d9cae48186e149cdf7110d9
Ricka12 <114760481+Ricka12@users.noreply.github.com>
2024-04-10 19:25:06 +0300

    Pakeisti dalykai
    Net nežinau ka pasakyt, viskas blogai.

--- Optimizavimas2.cpp
+++ Optimizavimas2.cpp
@@ -8,14 +8,29 @@ struct duKint { //Vartotojo apibreztas kintamasis kad galima butu perduoti 2 kin
 	double xNaujas1, xNaujas2;
 };
 /////////////////////////////////////////////////////////////////////////////////////////////////
-bool hessePatikra(double x2, double x3) //gal implementuosiu dar nezinau ar reikia !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+bool hessePatikra(double x2, double x3) 
 {
 	return 2 * x3 >= 0 && 2 * x2 + 2 * x3 - 1 >= 0 && 2 * x2 >= 0;//grazina 1 jei hesse matrica teigiamai apibrezta
 }
-
+/////////////////////////////////////////////////////////////////////////////////////////////////
 double tiksloFunkcija(double x2, double x3) //tikslo funkcija su kuria yra dirbama
 {
 	return (x2 * x2 * x3 + x2 * x3 * x3 - x2 * x3) / 8;
+}
+/////////////////////////////////////////////////////////////////////////////////////////////////
+double minimizuojamGamma(double gamma, double x1, double x2, double a1, double a2)
+{
+	if (x1 - gamma * a1 < x2 - gamma * a2 && gamma >= 0)
+	{
+		return x1 - gamma * a1;
+	}
+	else if (x1 - gamma * a1 > x2 - gamma * a2 && gamma >= 0)
+	{
+		return x2 - gamma * a2;
+	}
+	else {
+		return 0;
+	}
 }
 /////////////////////////////////////////////////////////////////////////////////////////////////
 duKint gradientas(duKint x) // skaiciuojamas gradientas nuo tasku
@@ -27,29 +42,22 @@ duKint gradientas(duKint x) // skaiciuojamas gradientas nuo tasku
 	return a;
 }
 /////////////////////////////////////////////////////////////////////////////////////////////////Intervalo dalijimo pusiau metodas
-double skaiciuojamFunkcija(double x)
+double intervaloPusiau(double gamma, duKint a)
 {
-	return ((x * x - 2) * (x * x - 2)) / 4 - 1;
-}
-////////////////////// Kaip pasiskaiciuot gamma?? kas yra gama siuo atveju?? !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-void intervaloPusiau()
-{
-	int pabaiga = 0, skaiciavimuKiekisDalijimoPusiau = 0, iteracijuSkDalPus = 0;
+	int pabaiga = 0;
 	double l = 0, r = 10;
 	double xm = (l + r) / 2;
 	double L = r - l;
 	double x1, x2;
 	double fxm, fx1, fx2;
-	fxm = skaiciuojamFunkcija(xm);
-	skaiciavimuKiekisDalijimoPusiau++;
+	fxm = minimizuojamGamma(xm, a.xNaujas1, a.xNaujas2, gradientas(a).xNaujas1, gradientas(a).xNaujas2);
 	while (pabaiga != 1)
 	{
-		iteracijuSkDalPus++;
 		x1 = l + L / 4;
 		x2 = r - L / 4;
-		fx1 = skaiciuojamFunkcija(x1);
-		fx2 = skaiciuojamFunkcija(x2);
-		skaiciavimuKiekisDalijimoPusiau += 2;
+		
+		fx1 = minimizuojamGamma(x1, a.xNaujas1, a.xNaujas2, gradientas(a).xNaujas1, gradientas(a).xNaujas2);
+		fx2 = minimizuojamGamma(x2, a.xNaujas1, a.xNaujas2, gradientas(a).xNaujas1, gradientas(a).xNaujas2);
 		if (fx1 < fxm)
 		{
 			r = xm;
@@ -73,6 +81,17 @@ void intervaloPusiau()
 			pabaiga = 1;
 		}
 	}
+	if (fx1 <= fxm && fx1 <= fx2)
+	{
+		return fx1;
+	}
+	else if (fxm <= fx1 && fxm <= fx2)
+	{
+		return fxm;
+	}
+	else {
+		return fx2;
+	}
 }
 /////////////////////////////////////////////////////////////////////////////////////////////////
 void gradientinisNusileidimas(duKint a)
@@ -87,7 +106,7 @@ void gradientinisNusileidimas(duKint a)
 		R << "plot3(" << a.xNaujas1 << ", " << a.xNaujas2 << ", " << tiksloFunkcija(a.xNaujas1, a.xNaujas2) << ",'ro', 'MarkerSize', 5)" << endl;// Vizualizcijai su octave
 		a.xNaujas1 = a.xNaujas1 - gamma * gradientas(a).xNaujas1;
 		a.xNaujas2 = a.xNaujas2 - gamma * gradientas(a).xNaujas2;
-		if (zingsnis.xNaujas1 - a.xNaujas1 <= 0.0000000001 && zingsnis.xNaujas2 - a.xNaujas2 <= 0.0000000001)//Iki kokio min zingsnio turi eiti?? !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+		if (zingsnis.xNaujas1 - a.xNaujas1 <= 0.0000000001 && zingsnis.xNaujas2 - a.xNaujas2 <= 0.0000000001)
 		{
 			pabaiga = 1;
 		}
@@ -97,21 +116,21 @@ void gradientinisNusileidimas(duKint a)
 void greiciausiasNusileidimas(duKint a)
 {
 	duKint zingsnis = a;
-	double gamma;
+	double gamma = 1;
 	int pabaiga = 0;
 	srand(time(NULL));
 	while (pabaiga != 1)
 	{
-		gamma = rand()%1+1; //!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 		zingsnis.xNaujas1 = a.xNaujas1;
 		zingsnis.xNaujas2 = a.xNaujas2;
-		R << "plot3(" << a.xNaujas1 << ", " << a.xNaujas2 << ", " << tiksloFunkcija(a.xNaujas1, a.xNaujas2) << ",'ro', 'MarkerSize', 5)" << endl;// Vizualizcijai su octave
+		R << "plot3(" << a.xNaujas1 << ", " << a.xNaujas2 << ", " << tiksloFunkcija(a.xNaujas1, a.xNaujas2) << ",'bo', 'MarkerSize', 5)" << endl;// Vizualizcijai su octave
 		a.xNaujas1 = a.xNaujas1 - gamma * gradientas(a).xNaujas1;
 		a.xNaujas2 = a.xNaujas2 - gamma * gradientas(a).xNaujas2;
-		if (zingsnis.xNaujas1 - a.xNaujas1 <= 0.0000000001 && zingsnis.xNaujas2 - a.xNaujas2 <= 0.0000000001)//Iki kokio min zingsnio turi eiti??
+		if (zingsnis.xNaujas1 - a.xNaujas1 <= 0.0000000001 && zingsnis.xNaujas2 - a.xNaujas2 <= 0.0000000001)
 		{
 			pabaiga = 1;
 		}
+		gamma = intervaloPusiau(gamma, a);
 	}
 }
 /////////////////////////////////////////////////////////////////////////////////////////////////
@@ -126,22 +145,24 @@ int main()
 	R << "fx = 1/8 * (x2.^2 .* x3 + x2 .* x3.^2 - x2 .* x3)" << endl;
 	R << "surf(x2, x3, fx)" << endl;
 	R << "title('Funkcijos vizualizacija')" << endl;
-	R << "xlabel('X-axis')" << endl;
-	R << "ylabel('Y-axis')" << endl;
-	R << "zlabel('Z-axis')" << endl;
+	R << "xlabel('X2-axis')" << endl;
+	R << "ylabel('X3-axis')" << endl;
+	R << "zlabel('Fx-axis')" << endl;
 
 	R << "hold on" << endl;
 	////////////////////////////////////////////////////////////////////////////////////gradientinisNusileidimas
-	duKint a{0,0};
+	duKint a{ 0,0 };
 	duKint b{ 1,1 };
-	duKint c{0.2,0.4};
+	duKint c{ 0.2,0.4 };
+	/*R << "% Gradientinis nusileidimas" << endl;
 	R << "% Taskas(0, 0)" << endl;
 	gradientinisNusileidimas(a);
 	R << "% Taskas(1, 1) " << endl;
 	gradientinisNusileidimas(b);
 	R << "% Taskas(0.2, 0.4) " << endl;
-	gradientinisNusileidimas(c);
+	gradientinisNusileidimas(c);*/
 	////////////////////////////////////////////////////////////////////////////////////greiciausiasNusileidimas
+	R << "% Greičiausias nusileidimas" << endl;
 	R << "% Taskas(0, 0)" << endl;
 	greiciausiasNusileidimas(a);
 	R << "% Taskas(1, 1) " << endl;
